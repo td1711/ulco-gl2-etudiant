f4194ed192cc48f2143ca6357361f9992feaa3dd
TODO <TODO>
2024-02-21 16:08:24 +0100

    add fibo iter

--- tp-ffi/py-fibo2/cpp/myfibo.cpp
+++ tp-ffi/py-fibo2/cpp/myfibo.cpp
@@ -5,12 +5,33 @@ int fiboNaive(int n) {
     return n < 2 ? n : fiboNaive(n-1) + fiboNaive(n-2);
 }
 
+
+int fiboIter(int n) {
+    assert(n => 0);
+    int n1 = 0;
+    int n2 = 1;
+    int fibo = 2;
+    if(n <= 1)
+        return n;
+    for(int i=2; i<n+1; i++){
+        fibo = n1 + n2;
+        n1 = n2;
+        n2 = fibo;
+    }
+    return fibo;
+
+}
+
 // TODO implement fiboIterative
 
 
 #include <pybind11/pybind11.h>
 
 PYBIND11_MODULE(myfibo, m) {
+
+    m.def("fibo_naive", &fiboNaive);
+    m.def("fibo_iter", &fiboIter);
+
 
     // TODO export fiboNaive (as fibo_naive)
 
--- tp-ffi/py-fibo2/scripts/main1.py
+++ tp-ffi/py-fibo2/scripts/main1.py
@@ -1,9 +1,12 @@
-
+import myfibo
 if __name__ == '__main__':
 
-    print('TODO')
-    
-    # TODO print fibo_naive(i) for i from 0 to 10
+    for i in range(11):
+        print(f"fibo_naive({i}) = {myfibo.fibo_naive(i)}")
+
+    print()
+    for i in range(11):
+        print(f"fibo_iter({i}) = {myfibo.fibo_iter(i)}")
 
     # TODO print fibo_iterative(i) for i from 0 to 10
 
