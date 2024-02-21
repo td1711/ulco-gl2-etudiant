49f41bb7b700bca2e2c2c5fa9d2d76cbb575d07a
TODO <TODO>
2024-02-21 15:47:55 +0100

    Update

--- tp-ffi/py-fibo1/scripts/main.py
+++ tp-ffi/py-fibo1/scripts/main.py
@@ -2,13 +2,11 @@ from mypackage.myfibo import fibo_naive, fibo_iterative
 
 if __name__ == '__main__':
 
-    print("fibo naive")
     for i in range(11):
-        print(fibo_naive(i))
+        print(f"fibo_naive({i}) = {fibo_naive(i)}")
 
     print()
-    print("fibo iterative")
     for i in range(11):
-        print(fibo_iterative(i))
+        print(f"fibo_iterative({i}) = {fibo_iterative(i)}")
 
 
