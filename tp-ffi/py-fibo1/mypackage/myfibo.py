fc7d98fb856c5c92cf4d6e5767512e91ee4e5764
TODO <TODO>
2024-02-21 15:42:19 +0100

    Add fibo_iterative funtion

--- tp-ffi/py-fibo1/mypackage/myfibo.py
+++ tp-ffi/py-fibo1/mypackage/myfibo.py
@@ -3,5 +3,15 @@ def fibo_naive(n: int):
     assert n >= 0
     return n if n < 2 else fibo_naive(n-1) + fibo_naive(n-2)
 
-# TODO implement fibo_iterative
+def fibo_iterative(n):
+    n1 = 1
+    n2 = 1
+    fibo = 2
+    if(n <= 1):
+        return n
+    for i in range(2,n):
+        fibo = n1 + n2
+        n1 = n2
+        n2 = fibo
+    return fibo
 
--- tp-ffi/py-fibo1/scripts/main.py
+++ tp-ffi/py-fibo1/scripts/main.py
@@ -1,11 +1,14 @@
-from mypackage.myfibo import fibo_naive
+from mypackage.myfibo import fibo_naive, fibo_iterative
 
 if __name__ == '__main__':
 
+    print("fibo naive")
     for i in range(11):
         print(fibo_naive(i))
-    
-    # TODO print fibo_naive(i) for i from 0 to 10
 
-    # TODO print fibo_iterative(i) for i from 0 to 10
+    print()
+    print("fibo iterative")
+    for i in range(11):
+        print(fibo_iterative(i))
+
 
