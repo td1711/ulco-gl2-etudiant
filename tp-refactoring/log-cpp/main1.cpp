8e59e5bd0c6ed2cfb76f642923906fc497b0d6a6
TODO <TODO>
2024-03-11 15:12:39 +0100

    Update

--- tp-refactoring/log-cpp/main1.cpp
+++ tp-refactoring/log-cpp/main1.cpp
@@ -18,10 +18,25 @@ int mycompute(int v0) {
     return v2;
 }
 
+
+int mycomputefile(int v0){
+    std::ofstream file;
+    file.open("message.txt");
+    file << "add3 " + std::to_string(v0) << std::endl;
+    const int v1 = add3(v0);
+    file << "mul2 " + std::to_string(v1) << std::endl;
+    const int v2 = add3(v1);
+    file.close();
+    return v2;
+}
+
 int main() {
     std::cout << "this is log-cpp" << std::endl;
 
     const int res = mycompute(18);
+    std::cout << res << std::endl;
+
+    const int resFile = mycomputefile(18);
     std::cout << res << std::endl;
 
     return 0;
