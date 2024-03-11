884cdabddbc0b830e1b24d8b1ed0edf8af182867
TODO <TODO>
2024-03-11 15:19:48 +0100

    main 2 log-cpp

--- tp-refactoring/log-cpp/main2.cpp
+++ tp-refactoring/log-cpp/main2.cpp
@@ -12,8 +12,22 @@ int mul2(int n) {
     return n*2;
 }
 
+int mycomputefile(std::ostream &o, int v0){
+    o << "add3 " + std::to_string(v0) << std::endl;
+    const int v1 = add3(v0);
+    o << "mul2 " + std::to_string(v1) << std::endl;
+    const int v2 = add3(v1);
+    return v2;
+}
+
 int main() {
     std::cout << "this is log-cpp" << std::endl;
+
+    mycomputefile(std::cout, 18);
+
+    std::ofstream of;
+    of.open("message.txt");
+    mycomputefile(of, 18);
 
     return 0;
 }
