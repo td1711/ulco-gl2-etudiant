c6ea5dd2c663c658f54892c7c38370f2890e1209
TODO <TODO>
2024-04-03 15:20:24 +0200

    Add menu

--- tp-mvc/todolist-cpp/app/main.cpp
+++ tp-mvc/todolist-cpp/app/main.cpp
@@ -1,11 +1,36 @@
 #include <todolist-cpp/Task.hpp>
 
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <todolist-cpp/View.hpp>
 
-int main() {
 
-    Task t {1, "TODO"};
-    std::cout << t._name << std::endl;
+int main(){
+    Board b;
+
+    while(true){
+        std::cout << showBoard(b) << std::endl;
+        std::string response, cmd, rst;
+        std::getline(std::cin, response);
+
+        if(response == "quit"){
+            break;
+        }
+
+        std::istringstream iss(response);
+        std::getline(iss, cmd, ' ');
+        std::getline(iss, rst);
+
+        
+        if(cmd == "add"){
+            b.addTodo(rst);
+        }
+        if(cmd == "done"){
+            b.toDone(std::stoi(rst));
+        }
+
+    }
 
     return 0;
 }
--- tp-mvc/todolist-cpp/test/main.cpp
+++ tp-mvc/todolist-cpp/test/main.cpp
@@ -1,6 +1,2 @@
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
-#include <todolist-cpp/View.hpp>
-#include <todolist-cpp/Task.hpp>
-
-void main()
