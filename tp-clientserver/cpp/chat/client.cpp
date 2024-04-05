9bdc26582f91fc807b3644e1f3f0b55c67946ea5
TODO <TODO>
2024-04-05 16:42:53 +0200

    Create chat between 2 clients with a server

--- tp-clientserver/cpp/chat/client.cpp
+++ tp-clientserver/cpp/chat/client.cpp
@@ -8,18 +8,27 @@ int main(int argc, char** argv) {
 
     hv::WebSocketClient ws;
     ws.onopen = []() {
-        std::cout << "connected" << std::endl;
+        std::cout << "----- connected -----" << std::endl;
     };
     ws.onmessage = [](const std::string& msg) {
-        std::cout << "received: " << msg << std::endl;
+        std::cout << msg << std::endl;
     };
     ws.onclose = []() {
-        std::cout << "disconnected" << std::endl;
+        std::cout << "----- disconnected -----" << std::endl;
         exit(0);
     };
     ws.open("ws://127.0.0.1:9000");
 
+    std::string input;
     while (true) {
+        std::getline(std::cin, input);
+        
+        if (input == "quit") {
+            ws.close();
+            break;
+        }
+
+        ws.send(input);
         std::this_thread::sleep_for(std::chrono::milliseconds(200));
     }
 
